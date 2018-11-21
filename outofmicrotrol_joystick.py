f5268d2d6cdbaa251914d7abd12dd2939f3a5bf4
Sarah Townson <sarah.townson@scienceoxford.com>
2018-11-21 15:33:55 +0000

    Update and rename robot_joystick.py to outofmicrotrol_joystick.py

--- outofmicrotrol_joystick.py
+++ outofmicrotrol_joystick.py
@@ -1,20 +1,57 @@
 from microbit import *
 import radio
 
+
+'''
+RADIO CONFIGURATION
+IMPORTANT: change the channel/address/group for each robot &
+           ensure the channel/address/group match the controller
+'''
 radio.config(channel=12)
 radio.config(address=0x12073120)
 radio.config(group=31)
 
 radio.on()
 
-# Please tag us if used!
-# We'd love to see what you make:
-# @ScienceOxford
 
-# Line-following robot code for microbit with L9110s motor driver
-# Thanks to MultiWingSpan, whose code for the Bit:Bot was a great starting point
-# http://www.multiwingspan.co.uk/micro.php?page=botline
+'''
+MCROBOFACE NEOPIXELS
+https://4tronix.co.uk/blog/?p=1383
+'''      
+mrf = neopixel.NeoPixel(pin0, 17)
+smile = [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1]
+frown = [1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1]
+line = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
+angry = [1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
 
+def exp(expression):
+    red_nose = random.randint(0, 255)
+    green_nose = random.randint(0, 255)
+    blue_nose = random.randint(0, 255)
+    red_eyes = random.randint(0, 255)
+    green_eyes = random.randint(0, 255)
+    blue_eyes = random.randint(0, 255)
+    red = random.randint(0, 255)
+    green = random.randint(0, 255)
+    blue = random.randint(0, 255)
+
+    mrf[14] = (red_nose, green_nose, blue_nose)
+    for id in range(15, 17):
+        mrf[id] = (red_eyes, green_eyes, blue_eyes)
+    for id in range(0, 14):
+        if (expression[id] != 0):
+            mrf[id] = (red, green, blue)
+        else:
+            mrf[id] = (0, 0, 0)
+
+    mrf.show()
+
+
+'''
+MOTOR CONTROL
+@ScienceOxford code for microbit with L9110s motor driver
+http://www.multiwingspan.co.uk/micro.php?page=botline
+'''
 LF = pin14
 LB = pin13
 RF = pin12
@@ -26,11 +63,13 @@ def stop():
     LB.write_analog(1023)
     RF.write_analog(1023)
     RB.write_analog(1023)
-    display.show(Image.ANGRY)
+    display.show(Image.HAPPY)
+    exp(smile)
 
 # Inputs between 0-1023 to control both motors
 def drive(L, R):
-    display.clear()
+    display.show(Image.ANGRY)
+    exp(angry)
     # Below controls the left wheel: forward, backward, stop at given speed
     if L > 0 and L <= 1023:
         LF.write_analog(abs(L-1023))  # go forwards at speed given
@@ -51,7 +90,12 @@ def drive(L, R):
     else:
         RF.write_analog(1023)         # stop the right wheel
         RB.write_analog(1023)
-        
+
+
+'''
+MAIN LOOP
+Receiving messages from controller_joystick.py
+'''
 while True:
     message = radio.receive()
     if message is not None:

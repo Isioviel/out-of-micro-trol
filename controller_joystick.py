5f0399de8f3894093703f48614de37f1bc7b9537
Sarah Townson <sarah.townson@scienceoxford.com>
2018-11-21 13:52:07 +0000

    Create controller_joystick.py

---
+++ controller_joystick.py
@@ -0,0 +1,48 @@
+from microbit import *
+import radio
+
+radio.config(channel=12)
+radio.config(address=0x12073120)
+radio.config(group=31)
+
+on = False
+
+display.show(Image.NO)
+
+buttons = {2: 'A',
+           516: 'B',
+           684: 'C',
+           768: 'D',
+           853: 'E',
+           819: 'F'}
+
+def button_press():
+    press = pin2.read_analog()
+    if press < 900:
+        for number in range(press-5, press+5):
+            if number in buttons:
+                return buttons[number]
+
+def drive():
+    x = (pin0.read_analog()) - 511
+    y = (pin1.read_analog()) - 511
+    left = y + x
+    right = y - x
+    return str(left) + " " + str(right)
+
+while True:
+    button = button_press()
+    if button == 'F':
+        radio.on()
+        display.show(Image.YES)
+        on = True
+    while on is True:
+        button = button_press()
+        if button == 'E':
+            radio.send('stop')
+            radio.off()
+            display.show(Image.NO)
+            on = False
+        else:
+            radio.send(drive())
+            sleep(10)
